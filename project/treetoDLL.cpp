f5ba669ac8f024d1109bfe2a26eeb11c14bdba2d
puneetagg <puneetaggarwal005@gmail.com>
2014-01-05 02:30:43 +0530

    Adding tree to DLL prorgram.

---
+++ project/treetoDLL.cpp
@@ -0,0 +1,33 @@
+#include"binarytree.h"
+
+void moveroottohead(node **root)
+{
+	while ((*root)->l != NULL)
+		*root = (*root)->l;
+}
+
+void treetoDLL(node* root,node **prev)
+{
+	if (root == NULL)
+		return;
+	treetoDLL(root->l,prev);
+	if (root->l != NULL)
+		root->l = *prev;
+	if ((*prev)!=NULL)
+		(*prev)->r = root;
+	*prev = root;
+	treetoDLL(root->r, prev);
+//	free(prev);
+
+}
+
+void main()
+{
+	node *root = createtree();
+	node* prev = NULL;
+	treetoDLL(root,&prev);
+
+	moveroottohead(&root);
+	print_t_dll(root);
+	getch();
+}
